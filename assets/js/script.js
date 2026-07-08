3791b355dedc58b63e733778d560695a5316b90e
Whyttin <web@whytt.info>
2026-07-08 13:35:02 -0400

    QOL Updates
    Bunch of usability updates, adds new svgs for external link support on the buttons, changes it so the footer and the last button don't overlap on small monitors, added the ability to have mobile support (hopefully this doesn't break).

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// get your element with its ID
+// const myElement = document.querySelector("footer") 
+// get current hour (24)
+// let timedate = new Date().getDate()
+// let timemonth = new Date().getMonth()
+
+// if (timedate > 5){
+//   myElement.classList.remove("hidden")
+// }else{
+//   myElement.classList.add("hidden");
+// }
